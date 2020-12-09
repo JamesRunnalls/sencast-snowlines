5546309996c64b9caea821ddfa24bf8004679a57
James Runnalls <james.runnalls@eawag.ch>
2020-12-09 10:30:20 +0100

    Add test

---
+++ tests/snowlines_test.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Import libs, some of them might be unused here, but something magical happens when they are imported
+# which causes geos_c.dll and objectify.pyx errors to disappear in windows.
+import cartopy.crs
+import netCDF4
+
+import sys
+sys.path.append("/home/ubuntu/sencast-snowlines")
+
+from main import hindcast
+
+hindcast("/home/ubuntu/sencast-snowlines/parameters/snowline_run.ini")
